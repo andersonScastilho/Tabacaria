fc249c2dc0bbf140bcf565761e2330eb1a45f1fa
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-11-03 09:59:33 -0300

    refactor: change the css code

--- src/component/categories/categories.component.js
+++ src/component/categories/categories.component.js
@@ -1,22 +1,22 @@
-import { useContext, useEffect } from "react"
-import { CategoryContext } from "../../contexts/categories/categories.context"
-import CategoryItem from "../category/category-item"
-import { CategoriesContainer } from "./categories.styles"
+import { useContext, useEffect } from "react";
+import { CategoryContext } from "../../contexts/categories/categories.context";
+import CategoryItem from "../category/category-item";
+import { CategoriesContainer } from "./categories.styles";
 
 const Categories = () => {
-    const { categories, fetchCategories } = useContext(CategoryContext)
+  const { categories, fetchCategories } = useContext(CategoryContext);
 
-    useEffect(() => {
-        fetchCategories()
-    })
+  useEffect(() => {
+    fetchCategories();
+  });
 
-    return (
-        <CategoriesContainer>
-            {categories.map((category) => (
-                <CategoryItem category={category} />
-            ))}
-        </CategoriesContainer>
-    )
-}
+  return (
+    <CategoriesContainer>
+      {categories.map((category) => (
+        <CategoryItem category={category} />
+      ))}
+    </CategoriesContainer>
+  );
+};
 
-export default Categories
+export default Categories;
--- src/component/category/category-item.styles.js
+++ src/component/category/category-item.styles.js
@@ -1,11 +1,33 @@
 import styled from "styled-components";
 
 export const CategoryContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  border: 2px solid black;
+  background-color: #fff;
+  border-radius: 5px;
+  background-image: ${(props) => `url("${props.backgroundImage}")`};
+  background-position: center;
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+export const CategoryDisplayName = styled.p`
+  color: #222;
+  font-weight: 600;
+  border: 2px solid transparent;
+  background: rgba(233, 236, 239, 0.45);
+  border-radius: 5px;
+  padding: 5px;
+  transition: 400ms;
+  &:hover {
+    transform: scale(1.1);
+    cursor: pointer;
     border: 2px solid black;
-    background-color: #fff;
-    border-radius:5px;
-    padding: 5px;
-    &:hover{
-        cursor: pointer;
-    }
-`
+  }
+`;
