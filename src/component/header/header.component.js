0d256fc96f693e4d85add08e24e17b6cf0ad3bff
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-11-05 13:31:12 -0300

    style: remove background and menu

--- src/component/header/header.component.js
+++ src/component/header/header.component.js
@@ -1,22 +1,11 @@
 import React from "react";
-
-import {
-  HeaderContainer,
-  ItemMenu,
-  MenuHeader,
-  LogoHeader,
-  TitleHeader,
-} from "./header.styles";
+import MaximusImage from "../../assets/Maximus_Image.jpeg";
+import { HeaderContainer, TitleHeader } from "./header.styles";
 
 const Header = () => {
   return (
-    <HeaderContainer>
-      <LogoHeader src="https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg" />
+    <HeaderContainer imageUrl={MaximusImage}>
       <TitleHeader>Maximus LoungeBar</TitleHeader>
-      <MenuHeader>
-        <ItemMenu>Desenvolvendo</ItemMenu>
-        <ItemMenu>Desenvolvendo</ItemMenu>
-      </MenuHeader>
     </HeaderContainer>
   );
 };
--- src/component/header/header.styles.js
+++ src/component/header/header.styles.js
@@ -1,19 +1,26 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
+  background-color: #222;
   display: flex;
   align-items: center;
   gap: 15px;
-  background-color: #3a3132;
   padding: 10px;
-  justify-content: space-between;
+  border-radius: 2px;
+  box-shadow: 1px 1px 1px #222;
+  justify-content: center;
+  flex-wrap: wrap;
+  @media (max-width: 823px) {
+    justify-content: center;
+  }
 `;
 export const LogoHeader = styled.img`
   height: 6rem;
   width: 10rem;
+  border-radius: 5px;
 `;
 export const TitleHeader = styled.h1`
-  color: #e30224;
+  color: #fff;
 `;
 export const MenuHeader = styled.menu`
   display: flex;
@@ -21,11 +28,12 @@ export const MenuHeader = styled.menu`
   padding: 5px;
 `;
 export const ItemMenu = styled.div`
+  background-color: #dce0e6;
   border-radius: 5px;
   border: 2px solid transparent;
   padding: 3px;
   box-shadow: 2px 2px 2px #222;
-  color: #fff;
+  color: #222;
   &:hover {
     cursor: pointer;
     border: 2px solid black;
