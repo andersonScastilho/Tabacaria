3769ae70683f44838c7e1f657887f222ca3dd3ca
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-11-05 13:31:53 -0300

    feat: use the component of login

--- src/component/categories/categories.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const CategoriesContainer = styled.div`
-  height: 100%;
-  display: flex;
-  padding: 15px;
-  justify-content: center;
-  align-items: center;
-`;
--- src/contexts/categories/categories.context.js
+++ src/contexts/categories/categories.context.js
@@ -5,14 +5,16 @@ import { useEffect } from "react";
 
 export const CategoryContext = createContext({
   categories: [],
+  isLoading: false,
   fetchCategories: () => Promise.resolve(),
 });
 
 const CategoryContextProvider = ({ children }) => {
   const [categories, setCategories] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
   const fetchCategories = async () => {
     try {
+      setIsLoading(true);
       const categoriesFromFirestore = [];
 
       const querySnapshot = await getDocs(collection(db, "Categories"));
@@ -20,17 +22,18 @@ const CategoryContextProvider = ({ children }) => {
       querySnapshot.forEach((doc) => {
         categoriesFromFirestore.push(doc.data());
       });
-
       setCategories(categoriesFromFirestore);
     } catch (error) {
       console.log({ error });
+    } finally {
+      setIsLoading(false);
     }
   };
   useEffect(() => {
     fetchCategories();
   }, []);
   return (
-    <CategoryContext.Provider value={{ categories, fetchCategories }}>
+    <CategoryContext.Provider value={{ categories, isLoading }}>
       {children}
     </CategoryContext.Provider>
   );
