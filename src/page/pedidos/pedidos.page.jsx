c85d16911d5a46e5479870383737b94be7dcef61
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-11-08 18:24:18 -0300

    feat: add page to pedidos

--- src/page/pedidos/pedidos.page.jsx
+++ src/page/pedidos/pedidos.page.jsx
@@ -1,10 +1,117 @@
-import { PedidoContainer } from "./pedidos.style";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { BsPlus } from "react-icons/bs";
+
+// import { AiOutlineMinus } from "react-icons/ai";
+import { CategoryContext } from "../../contexts/categories/categories.context";
+
+import Header from "../../component/header/header.component";
+import Footer from "../../component/footer/footer.component";
+import CustomButton from "../../component/custom-button/custom-button.component";
+import InputErrorMessage from "../../component/input-error-messag/input-error.component";
+
+import {
+  ButtonIcreaseProdut,
+  InputPedidos,
+  LabelInputPedidos,
+  PedidoContainer,
+  PedidoContent,
+  ProductsPrice,
+  ProdutosContainer,
+  ProdutosContent,
+  ProdutosPedidos,
+  TitleCategoryProducts,
+  TTeste,
+} from "./pedidos.style";
 
 const PedidosPage = () => {
+  const { categories } = useContext(CategoryContext);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  const handleSubmitPress = (data) => {
+    console.log(data);
+    setValue("nomeCliente", "");
+    setValue("mesaCliente", "");
+  };
   return (
-    <PedidoContainer>
-      <h2>Teste</h2>
-    </PedidoContainer>
+    <>
+      <Header />
+      <PedidoContainer>
+        <PedidoContent>
+          <LabelInputPedidos>Nome</LabelInputPedidos>
+          <InputPedidos
+            hasError={!!errors?.nomeCliente}
+            {...register("nomeCliente", { required: true })}
+          />
+          {errors?.nomeCliente?.type === "required" && (
+            <InputErrorMessage>
+              O nome do cliente é obrigaório
+            </InputErrorMessage>
+          )}
+          <LabelInputPedidos>Mesa</LabelInputPedidos>
+          <InputPedidos
+            type="number"
+            min={1}
+            hasError={!!errors?.mesaCliente}
+            {...register("mesaCliente", { required: true, min: 0 })}
+          />
+          {errors?.mesaCliente?.type === "required" && (
+            <InputErrorMessage>Informe a mesa do cliente</InputErrorMessage>
+          )}
+          {errors?.mesaCliente?.type === "min" && (
+            <InputErrorMessage>Mesa invalida</InputErrorMessage>
+          )}
+          <CustomButton onClick={() => handleSubmit(handleSubmitPress)()}>
+            Adicionar pedido
+          </CustomButton>
+        </PedidoContent>
+        <ProdutosContainer>
+          {categories.map((item) => (
+            <ProdutosContent key={item.id}>
+              <TitleCategoryProducts key={item.id}>
+                {item.name}
+              </TitleCategoryProducts>
+              {item.name === "Rosh"
+                ? item.marks.map((item) =>
+                    item.products.map((item) => (
+                      <TTeste>
+                        <ProdutosPedidos key={item.id}>
+                          {item.name}
+                        </ProdutosPedidos>
+                        <ProductsPrice>R${item.price},00</ProductsPrice>
+                        <ButtonIcreaseProdut>
+                          <BsPlus size={15} />
+                        </ButtonIcreaseProdut>
+                      </TTeste>
+                    ))
+                  )
+                : item.tipes.map((item) =>
+                    item.name === "Drinks"
+                      ? item.products.map((item) => (
+                          <TTeste key={item.id}>
+                            <ProdutosPedidos key={item.id}>
+                              {item.name}
+                            </ProdutosPedidos>
+                            <ProductsPrice>R${item.price},00</ProductsPrice>
+                            <ButtonIcreaseProdut>
+                              <BsPlus size={15} />
+                            </ButtonIcreaseProdut>
+                          </TTeste>
+                        ))
+                      : null
+                  )}
+            </ProdutosContent>
+          ))}
+        </ProdutosContainer>
+      </PedidoContainer>
+      <Footer />
+    </>
   );
 };
 export default PedidosPage;
--- src/page/pedidos/pedidos.style.js
+++ src/page/pedidos/pedidos.style.js
@@ -2,4 +2,64 @@ import styled from "styled-components";
 
 export const PedidoContainer = styled.div`
   display: flex;
+  height: 100%;
+  background-image: ${(props) => `url('${props.imageUrl}')`};
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  justify-content: center;
+  gap: 25px;
+  padding: 15px;
+`;
+export const PedidoContent = styled.div`
+  display: flex;
+  gap: 5px;
+  flex-direction: column;
+`;
+export const LabelInputPedidos = styled.label`
+  color: #fff;
+  font-weight: 450;
+  line-height: 30px;
+`;
+export const InputPedidos = styled.input`
+  width: 200px;
+  background-color: #222;
+  color: #fff;
+  border: 2px solid transparent;
+  border-radius: 5px;
+`;
+export const ProdutosContainer = styled.div`
+  display: flex;
+  gap: 15px;
+`;
+export const TitleCategoryProducts = styled.p`
+  text-align: center;
+  color: #fff;
+  font-size: 20px;
+`;
+export const ProdutosContent = styled.div`
+  display: flex;
+  gap: 5px;
+  background-color: rgba(0, 0, 0, 0.3);
+  width: 200px;
+  flex-direction: column;
+  border-radius: 7px;
+  border: 2px solid black;
+  padding: 5px;
+`;
+export const ProdutosPedidos = styled.p`
+  color: #fff;
+  text-align: start;
+`;
+export const TTeste = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-radius: 5px;
+  padding: 3px;
+  border: 2px solid black;
+`;
+export const ButtonIcreaseProdut = styled.button`
+  cursor: pointer;
+`;
+export const ProductsPrice = styled.p`
+  color: #fff;
 `;
