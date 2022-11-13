de4f632b01f5cb2fac151b0d52af4459b80086c3
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-11-12 23:42:44 -0300

    refactor: select style to styled-component

--- src/page/request/request.page.jsx
+++ src/page/request/request.page.jsx
@@ -1,32 +1,92 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { RequestContext } from "../../contexts/request.context";
-import {
-  RequestContainer,
-  RequestContent,
-  RequestStatus,
-  RequestText,
-} from "./request.style";
+
 import Header from "../../component/header/header.component";
 import Footer from "../../component/footer/footer.component";
 
+import {
+  FilterSelect,
+  RequestContainer,
+  RequestContent,
+  OptionSelect,
+  RequestStatus,
+  RequestText,
+} from "./request.style";
+
 const RequestPage = () => {
   const { request } = useContext(RequestContext);
+
+  const requestPendente = request.filter((request) => {
+    return request.status === "pendente";
+  });
+  const requestRealizado = request.filter((request) => {
+    return request.status === "realizado";
+  });
+  const [area, setArea] = useState("valor1");
+
+  const navigate = useNavigate();
+
+  const handleDetailsPage = (requestId) => {
+    navigate(`/details/${requestId}`);
+  };
 
   return (
     <>
       <Header />
       <RequestContainer>
-        {request.map((request) => (
-          <RequestContent key={request.id} onClick={() => console.log(request)}>
-            <input type="checkbox" />
-            <RequestStatus status={request.status}>
-              {request.status}
-            </RequestStatus>
-            <RequestText>Nome: {request.nameClient}</RequestText>
-            <RequestText>Mesa: {request.tableClient}</RequestText>
-            <RequestText>Preço Pedido R${request.priceTotal}</RequestText>
-          </RequestContent>
-        ))}
+        <FilterSelect
+          name="select"
+          value={area}
+          onChange={(e) => setArea(e.target.value)}
+        >
+          <OptionSelect value="valor1">Pendente</OptionSelect>
+          <OptionSelect value="valor2">Realizado</OptionSelect>
+          <OptionSelect value="valor3">Todos</OptionSelect>
+        </FilterSelect>
+
+        {area === "valor1"
+          ? requestPendente.map((request) => (
+              <RequestContent
+                key={request.id}
+                onClick={() => handleDetailsPage(request.id)}
+              >
+                <RequestStatus status={request.status}>
+                  {request.status}
+                </RequestStatus>
+                <RequestText>Nome: {request.nameClient}</RequestText>
+                <RequestText>Mesa: {request.tableClient}</RequestText>
+                <RequestText>Preço Pedido R${request.priceTotal}</RequestText>
+              </RequestContent>
+            ))
+          : area === "valor2"
+          ? requestRealizado.map((request) => (
+              <RequestContent
+                key={request.id}
+                onClick={() => handleDetailsPage(request.id)}
+              >
+                <RequestStatus status={request.status}>
+                  {request.status}
+                </RequestStatus>
+                <RequestText>Nome: {request.nameClient}</RequestText>
+                <RequestText>Mesa: {request.tableClient}</RequestText>
+                <RequestText>Preço Pedido R${request.priceTotal}</RequestText>
+              </RequestContent>
+            ))
+          : request.map((request) => (
+              <RequestContent
+                key={request.id}
+                onClick={() => handleDetailsPage(request.id)}
+              >
+                <RequestStatus status={request.status}>
+                  {request.status}
+                </RequestStatus>
+                <RequestText>Nome: {request.nameClient}</RequestText>
+                <RequestText>Mesa: {request.tableClient}</RequestText>
+                <RequestText>Preço Pedido R${request.priceTotal}</RequestText>
+              </RequestContent>
+            ))}
       </RequestContainer>
       <Footer />
     </>
--- src/page/request/request.style.js
+++ src/page/request/request.style.js
@@ -1,23 +1,36 @@
 import styled from "styled-components";
 
 export const RequestContainer = styled.div`
+  background-image: url("https://wallpaperaccess.com/full/1975322.jpg");
   display: flex;
+  background-size: 100% 100%;
   flex-direction: column;
+  height: 100%;
   gap: 7px;
   padding: 10px;
 `;
+export const FilterSelect = styled.select`
+  width: 100px;
+  background: none;
+  border-radius: 5px;
+`;
+export const OptionSelect = styled.option``;
+
 export const RequestContent = styled.div`
   border: 2px solid black;
+  background-color: #fff;
+  opacity: 0.8;
   border-radius: 5px;
   padding: 10px;
   display: flex;
   justify-content: space-around;
   cursor: pointer;
+  &:hover {
+    transform: scale(1.01);
+  }
 `;
-export const RequestText = styled.p`
-  color: #222;
-`;
+export const RequestText = styled.p``;
 
 export const RequestStatus = styled.p`
-  color: ${(props) => (props.status === "pendente" ? "red" : "blue")};
+  color: ${(props) => (props.status === "pendente" ? "red" : "green")};
 `;
