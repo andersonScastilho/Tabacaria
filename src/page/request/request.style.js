7d0e2fe777034fcd43bf396fa45e20acc20d0e9b
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-11-14 18:54:23 -0300

    style: change style of select

--- src/page/request/request.style.js
+++ src/page/request/request.style.js
@@ -11,26 +11,45 @@ export const RequestContainer = styled.div`
 `;
 export const FilterSelect = styled.select`
   width: 100px;
+  color: white;
+  font-weight: 500;
   background: none;
   border-radius: 5px;
 `;
-export const OptionSelect = styled.option``;
-
+export const OptionSelect = styled.option`
+  font-weight: 600;
+  font-size: 13px;
+  :nth-child(1) {
+    color: red;
+  }
+  :nth-child(2) {
+    color: green;
+  }
+  &:nth-child(3) {
+    color: black;
+  }
+`;
+export const RequestsContent = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 15px;
+`;
 export const RequestContent = styled.div`
   border: 2px solid black;
   background-color: #fff;
-  opacity: 0.8;
+  display: flex;
+  flex-direction: column;
   border-radius: 5px;
   padding: 10px;
-  display: flex;
-  justify-content: space-around;
   cursor: pointer;
   &:hover {
     transform: scale(1.01);
   }
 `;
-export const RequestText = styled.p``;
-
+export const RequestText = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+`;
 export const RequestStatus = styled.p`
   color: ${(props) => (props.status === "pendente" ? "red" : "green")};
 `;
