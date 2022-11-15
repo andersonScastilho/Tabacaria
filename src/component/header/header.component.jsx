c8c4502a45810c4dd235e7e35da12f7a8a6eeb93
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-11-15 08:12:37 -0300

    fix: change style to styled component

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -3,7 +3,9 @@ import { useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
 import { signOut } from "firebase/auth";
 import { auth } from "../../config/firebase.config";
+import { RiLogoutBoxRLine } from "react-icons/ri";
 
+import CustomButton from "../custom-button/custom-button.component";
 import MaximusImage from "../../assets/Maximus_Image.jpeg";
 
 import { HeaderContainer, TitleHeader } from "./header.styles";
@@ -13,7 +15,11 @@ const Header = () => {
   return (
     <HeaderContainer imageUrl={MaximusImage}>
       <TitleHeader>Em densenvolvimento</TitleHeader>
-      {isAuthenticated && <button onClick={() => signOut(auth)}>Teste</button>}
+      {isAuthenticated && (
+        <CustomButton onClick={() => signOut(auth)}>
+          <RiLogoutBoxRLine size={15} />
+        </CustomButton>
+      )}
     </HeaderContainer>
   );
 };
--- src/component/header/header.styles.js
+++ src/component/header/header.styles.js
@@ -2,8 +2,11 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   text-align: center;
+  display: flex;
+  align-items: center;
+  padding: 7px;
+  justify-content: space-between;
   background-color: #222;
-  padding: 5px;
 `;
 export const TitleHeader = styled.h1`
   color: #fff;
