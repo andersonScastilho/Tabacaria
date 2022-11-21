93610c885bfab7d5fa5ec727cdfbe18cc4266174
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-11-21 13:53:53 -0300

    feat: taking total pedidos e lucro

--- src/page/fechamento/fechamento.page.jsx
+++ src/page/fechamento/fechamento.page.jsx
@@ -1,19 +1,100 @@
 import { useContext } from "react";
+import Footer from "../../component/footer/footer.component";
+import Header from "../../component/header/header.component";
 import { UserContext } from "../../contexts/user.context";
 import { RequestContext } from "../../contexts/request.context";
+import { useForm } from "react-hook-form";
+import {
+  FechamentoContainer,
+  FechamentoFilterContainer,
+} from "./fechamento.styles";
+import { useState } from "react";
 
 const FechamentoPage = () => {
-  const { currentUser, isAuthenticated } = useContext(UserContext);
+  // const { currentUser, isAuthenticated } = useContext(UserContext);
   const { request } = useContext(RequestContext);
 
-  const date = new Date();
+  const { register, handleSubmit } = useForm();
 
-  const currentDate = date.toLocaleDateString();
-  const currentHors = date.toLocaleTimeString();
+  const [requesFiltred, setRequestFiltred] = useState();
+
+  const TotalRequest = requesFiltred?.reduce((acc) => {
+    return acc + 1;
+  }, 0);
+
+  const PriceTotalRequest = requesFiltred?.reduce((acc, item) => {
+    return acc + item.priceTotal;
+  }, 0);
+
+  const handleSubmitPress = (data) => {
+    function adicionaZero(numero) {
+      if (numero <= 9) return "0" + numero;
+      else return numero;
+    }
+
+    let dateInicio = new Date(data.dateInicio); //29/01/2020
+    let horaInicio = `${data.horaInicio}:59`;
+
+    let dataInicioFormatada =
+      adicionaZero(dateInicio.getDate() + 1).toString() +
+      "/" +
+      adicionaZero(dateInicio.getMonth() + 1).toString() +
+      "/" +
+      dateInicio.getFullYear();
+
+    let dateFim = new Date(data.dateFim);
+
+    let horaFim = `${data.horaFim}:59`;
+
+    let dataFimFormatada =
+      adicionaZero(dateFim.getDate() + 1).toString() +
+      "/" +
+      adicionaZero(dateFim.getMonth() + 1).toString() +
+      "/" +
+      dateFim.getFullYear();
+
+    const requestDateTime = request.filter((request) => {
+      return (
+        request.currentDate >= dataInicioFormatada &&
+        request.currentDate <= dataFimFormatada &&
+        request.currentHors >= horaInicio &&
+        request.currentHors <= horaFim
+      );
+    });
+    setRequestFiltred(requestDateTime);
+  };
 
   return (
     <>
-      <h1>Teste</h1>
+      <Header />
+      <FechamentoContainer>
+        <FechamentoFilterContainer>
+          <label>Data inicio: </label>
+          <input
+            type="date"
+            {...register("dateInicio", {
+              required: true,
+            })}
+          />
+          <label htmlFor="">Hora inicio: </label>
+          <input type="time" {...register("horaInicio")} />
+
+          <label htmlFor="">Data fim: </label>
+          <input
+            type="date"
+            {...register("dateFim", {
+              required: true,
+            })}
+          />
+          <label htmlFor="">Hora fim: </label>
+          <input type="time" {...register("horaFim")} />
+
+          <button onClick={() => handleSubmit(handleSubmitPress)()}>
+            Teste
+          </button>
+        </FechamentoFilterContainer>
+      </FechamentoContainer>
+      <Footer />
     </>
   );
 };
---
+++ src/page/fechamento/fechamento.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const FechamentoContainer = styled.div`
+  display: flex;
+  flex: 1 0 auto;
+  padding: 10px;
+`;
+export const FechamentoFilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 2px solid black;
+  padding: 5px;
+  border-radius: 5px;
+  background-color: #222;
+  gap: 7px;
+  color: white;
+`;
