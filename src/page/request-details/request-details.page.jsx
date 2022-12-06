45c5d41bb59332186346f35b529a5daa4c6f3c90
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-12-06 19:32:58 -0300

    refactor: refactor fetchRequest and add onSnapshot

--- src/page/request-details/request-details.page.jsx
+++ src/page/request-details/request-details.page.jsx
@@ -7,9 +7,8 @@ import { GiTable } from "react-icons/gi";
 import { useForm } from "react-hook-form";
 
 import { db } from "../../config/firebase.config";
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, onSnapshot, updateDoc } from "firebase/firestore";
 import { UserContext } from "../../contexts/user.context";
-import { RequestContext } from "../../contexts/request.context";
 
 import Header from "../../component/header/header.component";
 import Footer from "../../component/footer/footer.component";
@@ -27,52 +26,68 @@ import {
   SelectOfPayment,
   OptionOfPayment,
 } from "./request-details.style";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const RequestDatilsPage = () => {
-  const { request } = useContext(RequestContext);
   const { currentUser } = useContext(UserContext);
 
   const { id } = useParams();
   const alert = useAlert();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const { register, handleSubmit } = useForm();
 
-  const currentRequest = request.filter((request) => {
-    return request.idFromFirestore === id;
+  const [currentRequest, setCurrentRequest] = useState({
+    currentDate: "",
+    currentHOrs: "",
+    formOfPayment: "",
+    nameClient: "",
+    observation: "",
+    paymentStats: "",
+    priceTotal: "",
+    products: [],
+    status: "",
+    tableClient: "",
   });
 
-  const allProductOfRequest = currentRequest[0]?.products.every((item) => {
+  const fetchCurrentRequest = async () => {
+    try {
+      onSnapshot(doc(db, "Pedidos", id), (doc) => {
+        setCurrentRequest(doc.data());
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchCurrentRequest();
+  }, []);
+
+  const allProductOfRequest = currentRequest?.products.every((item) => {
     return item.status === "realizado";
   });
 
   const handleFinalizeItem = async (product) => {
     const frankDocRef = doc(db, "Pedidos", id);
 
-    let dataProducts = [...currentRequest[0].products];
+    let dataProducts = [...currentRequest.products];
 
     let dataProductSemCurrentProduct = dataProducts.filter((item) => {
       return item.id !== product.id;
     });
 
-    let currentProduct = dataProducts.filter((item) => {
-      return item.id === product.id;
-    });
     if (product.status !== "realizado") {
       await updateDoc(frankDocRef, {
         products: [
           ...dataProductSemCurrentProduct,
-          (currentProduct = {
+          {
             id: product.id,
             imageUrl: product.imageUrl,
             name: product.name,
             price: product.price,
             quantity: product.quantity,
             status: "realizado",
-          }),
+          },
         ],
       });
       alert.success("O status do item foi alterado");
@@ -85,15 +100,15 @@ const RequestDatilsPage = () => {
 
     if (allProductOfRequest === true) {
       if (
-        currentRequest[0].formOfPayment !== "false" &&
-        currentRequest[0].paymentStats === "realizado"
+        currentRequest.formOfPayment !== "false" &&
+        currentRequest.paymentStats === "realizado"
       ) {
         await updateDoc(requestRef, {
           status: "finalizado",
         });
         alert.success("O status do pedido foi alterado");
       } else if (
-        currentRequest[0].formOfPayment === "false" &&
+        currentRequest.formOfPayment === "false" &&
         data.formOfPayment !== "false"
       ) {
         await updateDoc(requestRef, {
@@ -115,116 +130,114 @@ const RequestDatilsPage = () => {
     <>
       <Header />
       <DetailsRequestContainer>
-        {currentRequest.map((request) => (
-          <DetailsRequestContent key={id}>
-            <DataRequestContainer>
-              <DataRequestText>
-                <BsFillPersonFill color="black" size={25} />
-              </DataRequestText>
-              <DataRequestText>Cliente:</DataRequestText>
-              <DataRequestText>{request.nameClient}</DataRequestText>
-            </DataRequestContainer>
-            <DataRequestContainer>
-              <DataRequestText>
-                <GiTable color="black" size={29} />
-              </DataRequestText>
-              <DataRequestText>Mesa:</DataRequestText>
-              <DataRequestText>{request.tableClient}</DataRequestText>
-            </DataRequestContainer>
-            <DataRequestContainer>
-              <DataRequestText>
-                <BsEyeSlashFill />
-              </DataRequestText>
-              <DataRequestText>Observação:</DataRequestText>
-              <DataRequestText>{request.observation}</DataRequestText>
-            </DataRequestContainer>
-            {request.paymentStats === "pendente" ? (
-              <>
-                <SelectOfPayment
-                  {...register("formOfPayment", {
-                    required: true,
-                  })}
-                >
-                  <OptionOfPayment value="false"></OptionOfPayment>
-                  <OptionOfPayment value="dinheiro">Dinheiro</OptionOfPayment>
-                  <OptionOfPayment value="cartaoDebito">
-                    Cartão - Debito
-                  </OptionOfPayment>
-                  <OptionOfPayment value="cartaoCredito">
-                    Cartao - Credito
-                  </OptionOfPayment>
-                  <OptionOfPayment value="pix">Pix</OptionOfPayment>
-                </SelectOfPayment>
-              </>
-            ) : null}
-            <DataRequestContainer>
-              <DataRequestText>Pagamento</DataRequestText>
-              <StatusText status={request.paymentStats}>
-                {request.paymentStats}
-              </StatusText>
-              {currentRequest[0].paymentStats === "pendente" ? (
-                <DataRequestContainer key={request.id}>
-                  <DataRequestText style={{ position: "relative", top: "5px" }}>
-                    <BiX size={25} color="red" />
-                  </DataRequestText>
-                </DataRequestContainer>
-              ) : (
-                <DataRequestText>
-                  <BiCheck size={25} color="green" />
-                </DataRequestText>
-              )}
-            </DataRequestContainer>
-            <DataRequestContainer>
-              <DataRequestText>Status Pedido</DataRequestText>
-              <StatusText status={request.status}>{request.status}</StatusText>
-              {currentRequest[0].status === "em andamento" ? (
-                <DataRequestContainer key={request.id}>
-                  <DataRequestText style={{ position: "relative", top: "5px" }}>
-                    <BiX size={25} color="red" />
-                  </DataRequestText>
-                </DataRequestContainer>
-              ) : (
-                <DataRequestText>
-                  <BiCheck size={25} color="green" />
-                </DataRequestText>
-              )}
-            </DataRequestContainer>
-            <TitleProductText>Produtos</TitleProductText>
-            <RequestProductsContainer>
-              {currentRequest[0].products.map((product) => (
-                <RequestProductsContent key={product.id}>
-                  {product.mark ? (
-                    <DataRequestText>
-                      Item: {product.mark} - {product.name}
-                    </DataRequestText>
-                  ) : (
-                    <DataRequestText>Item: {product.name}</DataRequestText>
-                  )}
-                  <DataRequestText>
-                    Quantidade: {product.quantity}
-                  </DataRequestText>
-                  <DataRequestContainer>
-                    <DataRequestText>Status:</DataRequestText>
-                    <StatusText status={product.status}>
-                      {product.status}
-                    </StatusText>
-                  </DataRequestContainer>
-                  <CustomButton onClick={() => handleFinalizeItem(product)}>
-                    Finalizar item
-                  </CustomButton>
-                </RequestProductsContent>
-              ))}
-            </RequestProductsContainer>
-            Total Pedido: ${request.priceTotal}
-            {currentUser.email === "userteste@gmail.com" ? (
-              <CustomButton
-                onClick={() => handleSubmit(handleFinalizeRequest)()}
+        <DetailsRequestContent>
+          <DataRequestContainer>
+            <DataRequestText>
+              <BsFillPersonFill color="black" size={25} />
+            </DataRequestText>
+            <DataRequestText>Cliente:</DataRequestText>
+            <DataRequestText>{currentRequest.nameClient}</DataRequestText>
+          </DataRequestContainer>
+          <DataRequestContainer>
+            <DataRequestText>
+              <GiTable color="black" size={29} />
+            </DataRequestText>
+            <DataRequestText>Mesa:</DataRequestText>
+            <DataRequestText>{currentRequest.tableClient}</DataRequestText>
+          </DataRequestContainer>
+          <DataRequestContainer>
+            <DataRequestText>
+              <BsEyeSlashFill />
+            </DataRequestText>
+            <DataRequestText>Observação:</DataRequestText>
+            <DataRequestText>{currentRequest.observation}</DataRequestText>
+          </DataRequestContainer>
+          {currentRequest.paymentStats === "pendente" ? (
+            <>
+              <SelectOfPayment
+                {...register("formOfPayment", {
+                  required: true,
+                })}
               >
-                Finalizar Pedido
-              </CustomButton>
-            ) : null}
-          </DetailsRequestContent>
-        ))}
+                <OptionOfPayment value="false"></OptionOfPayment>
+                <OptionOfPayment value="dinheiro">Dinheiro</OptionOfPayment>
+                <OptionOfPayment value="cartaoDebito">
+                  Cartão - Debito
+                </OptionOfPayment>
+                <OptionOfPayment value="cartaoCredito">
+                  Cartao - Credito
+                </OptionOfPayment>
+                <OptionOfPayment value="pix">Pix</OptionOfPayment>
+              </SelectOfPayment>
+            </>
+          ) : null}
+          <DataRequestContainer>
+            <DataRequestText>Pagamento</DataRequestText>
+            <StatusText status={currentRequest.paymentStats}>
+              {currentRequest.paymentStats}
+            </StatusText>
+            {currentRequest.paymentStats === "pendente" ? (
+              <DataRequestContainer>
+                <DataRequestText style={{ position: "relative", top: "5px" }}>
+                  <BiX size={25} color="red" />
+                </DataRequestText>
+              </DataRequestContainer>
+            ) : (
+              <DataRequestText>
+                <BiCheck size={25} color="green" />
+              </DataRequestText>
+            )}
+          </DataRequestContainer>
+          <DataRequestContainer>
+            <DataRequestText>Status Pedido</DataRequestText>
+            <StatusText status={currentRequest.status}>
+              {currentRequest.status}
+            </StatusText>
+            {currentRequest.status === "em andamento" ? (
+              <DataRequestContainer>
+                <DataRequestText style={{ position: "relative", top: "5px" }}>
+                  <BiX size={25} color="red" />
+                </DataRequestText>
+              </DataRequestContainer>
+            ) : (
+              <DataRequestText>
+                <BiCheck size={25} color="green" />
+              </DataRequestText>
+            )}
+          </DataRequestContainer>
+          <TitleProductText>Produtos</TitleProductText>
+          <RequestProductsContainer>
+            {currentRequest.products.map((product) => (
+              <RequestProductsContent key={product.id}>
+                {product.mark ? (
+                  <DataRequestText>
+                    Item: {product.mark} - {product.name}
+                  </DataRequestText>
+                ) : (
+                  <DataRequestText>Item: {product.name}</DataRequestText>
+                )}
+                <DataRequestText>
+                  Quantidade: {product.quantity}
+                </DataRequestText>
+                <DataRequestContainer>
+                  <DataRequestText>Status:</DataRequestText>
+                  <StatusText status={product.status}>
+                    {product.status}
+                  </StatusText>
+                </DataRequestContainer>
+                <CustomButton onClick={() => handleFinalizeItem(product)}>
+                  Finalizar item
+                </CustomButton>
+              </RequestProductsContent>
+            ))}
+          </RequestProductsContainer>
+          Total Pedido: ${currentRequest.priceTotal}
+          {currentUser.email === "userteste@gmail.com" ? (
+            <CustomButton onClick={() => handleSubmit(handleFinalizeRequest)()}>
+              Finalizar Pedido
+            </CustomButton>
+          ) : null}
+        </DetailsRequestContent>
       </DetailsRequestContainer>
       <Footer />
     </>
