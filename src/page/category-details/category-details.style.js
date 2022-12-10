c81852be286b2fbb6d37b201a27943753c2481f7
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-12-10 16:57:18 -0300

    style: add responsiveness

--- src/page/category-details/category-details.style.js
+++ src/page/category-details/category-details.style.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 export const CategoryDetailsContainer = styled.div`
   align-items: center;
+  flex: 1 0 auto;
   padding: 5px;
+  background-color: chocolate;
   display: flex;
   flex-direction: column;
 `;
